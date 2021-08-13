8cebdfcf3dc5c786c2c0a8b21ddacfc2c4678a16
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-08-13 17:29:15 -0400

    passed all specs

--- server/db/Place.js
+++ server/db/Place.js
@@ -1,8 +1,35 @@
-const Sequelize = require('sequelize');
+const {Sequelize, VIRTUAL} = require('sequelize');
+const {STRING, ENUM} = Sequelize;
 const db = require('./db');
 
 const Place = db.define('place', {
+    place_name: {
+        type: STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    category: {
+        type: ENUM('CITY', 'STATE','COUNTRY'),
+        defaultValue: 'STATE',
+        allowNull: false
+    },
+    isState: {
+        type: VIRTUAL,
+        get() {
+            return this.category === 'STATE';
+        }
+    },
+    nickname: {
+        type: VIRTUAL,
+        get() {
+            return this.place_name.split(' ').map(word => word[0].toUpperCase()).join('');
+        }
+    }
 });
+
 
 /**
  * We've created the association for you!
@@ -19,5 +46,31 @@ const Place = db.define('place', {
 
 Place.belongsTo(Place, { as: 'parent' });
 Place.hasMany(Place, { as: 'children', foreignKey: 'parentId' });
+
+//parentId will be added to Place
+//childrenId will be added to Place with the foreignKey of parentId
+
+//foreign key will be in place w alias childrenId using foreign key parentId?
+
+Place.findCitiesWithNoParent = function(){
+    return this.findAll({
+        where: {
+            category: 'CITY',
+            parentId: null
+        }
+    });    
+}
+
+Place.findStatesWithCities = function(){
+    return this.findAll({
+        where: {
+            category: 'STATE'
+        },
+        include: {
+            model: Place,
+            as: 'children'
+        }
+    });
+}
 
 module.exports = Place;
--- server/routes/places.js
+++ server/routes/places.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
+const app = require('../app');
 const {
   models: { Place },
 } = require('../db');
 
 // Add your routes here:
 //
+router.get('/unassigned', async(req, res, next) => {
+  try {
+    res.send(await Place.findCitiesWithNoParent());
+  }
+  catch(err){
+    next(err);
+  }
+});
+
+router.get('/states', async(req, res, next) => {
+  try {
+    res.send (await Place.findStatesWithCities());
+  }
+  catch(err){
+    next(err);
+  }
+}); 
+
+router.delete('/:id', async(req, res, next) => {
+  const {id} = req.params;
+  await Place.destroy(id);
+  res.sendStatus(404);
+});
+
 module.exports = router;
